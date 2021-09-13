d4d31408b1f363baafed42e03ed9840a27458c7a
Main <q1470001@gmail.com>
2021-09-13 17:48:16 +0800

    add convertencode ver 1
    try remove compile file

---
+++ CodeConversionApp/EncodeConvert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeConversionApp
+{
+    public static class EncodeConvert
+    {
+        public static string Convert_BIG5_to_UTF8(string path, bool is_save_ori_file)
+        {
+            try
+            {
+                if (System.IO.File.Exists(path) == false)
+                {
+                    return "找不到檔案！";
+                }
+                System.IO.FileInfo this_file = new System.IO.FileInfo(path);
+                if (is_save_ori_file == true)
+                {
+                    string parent_path = this_file.Directory.FullName;
+                    string save_path = parent_path + @"\orifile";
+                    if (System.IO.Directory.Exists(save_path) == false)
+                    {
+                        System.IO.Directory.CreateDirectory(save_path);
+                    }
+                    System.IO.File.Copy(this_file.FullName, save_path + @"\" + this_file.Name);
+                }
+
+                var tmp_str = System.IO.File.ReadAllText(path, Encoding.GetEncoding("BIG5"));
+                System.IO.File.WriteAllText(path, tmp_str, Encoding.UTF8);
+
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            return "";
+        }
+    }
+}
--- CodeConversionApp/Form1.cs
+++ CodeConversionApp/Form1.cs
@@ -14,8 +14,64 @@ namespace CodeConversionApp
     {
         public Form1()
         {
+            // 初始化BIG5語系編碼
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
             InitializeComponent();
         }
 
+        private void btn_go_convert_Click(object sender, EventArgs e)
+        {
+            if (this.txt_path.Text == "")
+            {
+                MessageBox.Show("請選擇檔案/目錄");
+            }
+            else
+            {
+                string this_path = this.txt_path.Text;
+                if (System.IO.File.Exists(this_path) == true)
+                {
+                    //MessageBox.Show("檔案");
+                    var result = EncodeConvert.Convert_BIG5_to_UTF8(this_path, this.chk_save_ori_file.Checked);
+                    if (result != "")
+                    {
+                        MessageBox.Show($"轉換失敗!{result}");
+                    }
+                }
+                else if (System.IO.Directory.Exists(this_path) == true)
+                {
+                    //MessageBox.Show("目錄");
+                    List<string> file_list = General.GetFileList(this_path);
+                    foreach (var item in file_list)
+                    {
+                        var result = EncodeConvert.Convert_BIG5_to_UTF8(item, this.chk_save_ori_file.Checked);
+                        if (result != "")
+                        {
+                            MessageBox.Show($"轉換失敗!{result}");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("路徑錯誤");
+                }
+                MessageBox.Show($"轉換完畢!");
+            }
+        }
+
+        private void btn_select_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            this.txt_path.Text = file.FileName;
+        }
+
+        private void btn_select_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.txt_path.Text = path.SelectedPath;
+        }
     }
 }
